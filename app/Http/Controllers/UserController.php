c6b2bb61ea4f88f5fc2bd86a3950739d646bb9da
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-03-21 10:28:43 +0000

    ajouter functions register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,23 +1,39 @@
 <?php
-
 namespace App\Http\Controllers;
-
+  
+use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
-
+use Validator;
+  
+  
 class UserController extends Controller
 {
-    public function register($request)
-    {
-
-      $user =  User::create([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'password'=>$request->password,
-       ]);
-
-       return response()->json(['message'=>'user registered Succeccfully','user'=>$user],201);
-
-
+ 
+    /**
+     * Register a User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register() {
+        $validator = Validator::make(request()->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:8',
+        ]);
+  
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+  
+        $user = new User;
+        $user->name = request()->name;
+        $user->email = request()->email;
+        $user->password = bcrypt(request()->password);
+        $user->save();
+  
+        return response()->json($user, 201);
     }
+  
+  
+
 }
